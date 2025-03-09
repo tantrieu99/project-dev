41b4f44b36cddc7d889c2b20d173bf2d8d74ebeb
tantrieu <tantrieujr@gmail.com>
2025-03-10 01:47:10 +0700

    fix run build

--- components/element/translation.tsx
+++ components/element/translation.tsx
@@ -1,4 +1,4 @@
-export const translations = {
+export const translations: Record<string, Record<string, string>> = {
   en: {
     about: "About Us",
     games: "Games",
@@ -7,15 +7,15 @@ export const translations = {
     gettingReady: "We’re Getting Ready",
     gettingReadyMobile1: "We’re",
     gettingReadyMobile2: "Getting Ready",
-    weWill1: "We will back to something amazing. Getting the latest",
-    weWill2: "updates about our games. Please sign up to our newsletter.",
+    weWill1: "We will be back with something amazing. Get the latest",
+    weWill2: "updates about our games. Please sign up for our newsletter.",
     enterEmail: "Enter your email",
     desAbout:
       "Browse our selection of free online games and have a great time without leaving the site! Our Kids Games option also includes game reviews, extensive game cheats and walkthroughs, and much more. We have exclusive free downloads, videos, and articles as well. Etech reviews the most popular kids games from all the most popular video gaming platforms, so you don’t need to search around for fun anywhere else on the Internet. Explore a whole new world of gaming on Etech.",
     users: "Users",
     hour24: "24 - Hour",
     desHour24:
-      "24/7 access ensures operators' businesses runs smoothly all year long.",
+      "24/7 access ensures operators' businesses run smoothly all year long.",
     design: "Design",
     desDesign:
       "Combining imaginative universes, play dynamics, and unprecedented gameplay, our games transcend the boundaries of the virtual world by weaving innovative gameplay.",
@@ -23,21 +23,21 @@ export const translations = {
     desTeam:
       "Etech is an award-winning, international studio of designers, artists, animators and producers that create content for the biggest names in film and video games.",
     ourGames: "Our Games",
-    desOurGames: "As a pioneer of mobile app gamification, we take pride in originality and individuality, providing global players with state-of-the-art games that feature splendid storylines, sensational sound effects and magnificent animation that never cease to impress.",
-    desOurGames1: "As a pioneer of mobile app gamification, we take pride in originality and individuality, providing global players with",
-    desOurGames2: " state-of-the-art games that feature splendid storylines, sensational sound effects and magnificent animation that never",
-    desOurGames3: " cease to impress.",
+    desOurGames:
+      "As a pioneer of mobile app gamification, we take pride in originality and individuality, providing global players with state-of-the-art games that feature splendid storylines, sensational sound effects and magnificent animation that never cease to impress.",
     ourPartners: "Our Partners",
     address: "Address",
     address1: "Valletta Buildings, South Street, Valletta - VLT 1103 Malta, US",
-    address2: "20 Phan Dang Luu street, Hai Chau District, Danang city, Vietnam",
+    address2:
+      "20 Phan Dang Luu street, Hai Chau District, Danang city, Vietnam",
     or: "or",
     subscribe: "Subscribe",
-    desSubscribe: "Subscribe to our newsletter and be the first to know about our updates",
+    desSubscribe:
+      "Subscribe to our newsletter and be the first to know about our updates",
     days: "Days",
     hours: "Hours",
     minutes: "Minutes",
-    seconds: "Seconds"
+    seconds: "Seconds",
   },
   vi: {
     about: "Về chúng tôi",
@@ -59,25 +59,25 @@ export const translations = {
       "Đảm bảo hoạt động kinh doanh của các nhà khai thác hoạt động trơn tru suốt cả năm.",
     design: "Thiết Kế",
     desDesign:
-      "Kết hợp các vũ trụ giàu trí tưởng tượng, động lực chơi và lối chơi chưa từng có, trò chơi của chúng tôi vượt qua ranh giới của thế giới ảo bằng cách tạo ra lối chơi sáng tạo. ",
+      "Kết hợp các vũ trụ giàu trí tưởng tượng, động lực chơi và lối chơi chưa từng có, trò chơi của chúng tôi vượt qua ranh giới của thế giới ảo bằng cách tạo ra lối chơi sáng tạo.",
     team: "Đội Ngũ",
     desTeam:
       "Etech là một studio quốc tế, từng đoạt giải thưởng gồm các nhà thiết kế, nghệ sĩ, họa sĩ hoạt hình và nhà sản xuất chuyên tạo ra nội dung cho những tên tuổi lớn nhất trong lĩnh vực điện ảnh và trò chơi điện tử.",
     ourGames: "Trò chơi",
-    desOurGames: "Là nhà tiên phong trong lĩnh vực trò chơi ứng dụng dành cho thiết bị di động, chúng tôi tự hào về tính độc đáo và cá tính, cung cấp cho người chơi toàn cầu những trò chơi hiện đại có cốt truyện tuyệt vời, hiệu ứng âm thanh giật gân và hoạt ảnh hoành tráng không ngừng gây ấn tượng.",
-    desOurGames1: "Là nhà tiên phong trong lĩnh vực trò chơi ứng dụng dành cho thiết bị di động, chúng tôi tự hào về tính độc đáo và cá tính,",
-    desOurGames2: " cung cấp cho người chơi toàn cầu những trò chơi hiện đại có cốt truyện tuyệt vời, hiệu ứng âm thanh giật gân và hoạt ảnh",
-    desOurGames3: " hoành tráng không ngừng gây ấn tượng.",
+    desOurGames:
+      "Là nhà tiên phong trong lĩnh vực trò chơi ứng dụng dành cho thiết bị di động, chúng tôi tự hào về tính độc đáo và cá tính, cung cấp cho người chơi toàn cầu những trò chơi hiện đại có cốt truyện tuyệt vời, hiệu ứng âm thanh giật gân và hoạt ảnh hoành tráng không ngừng gây ấn tượng.",
     ourPartners: "Đối tác",
     address: "Địa chỉ",
     address1: "Valletta Buildings, South Street, Valletta - VLT 1103 Malta, Mỹ",
-    address2: "Số 20 đường Phan Đăng Lưu, quận Hải Châu, thành phố Đà Nẵng, Việt Nam",
+    address2:
+      "Số 20 đường Phan Đăng Lưu, quận Hải Châu, thành phố Đà Nẵng, Việt Nam",
     or: "hoặc",
     subscribe: "Theo dõi",
-    desSubscribe: "Đăng ký nhận bản tin của chúng tôi và là người đầu tiên biết về các cập nhật của chúng tôi",
+    desSubscribe:
+      "Đăng ký nhận bản tin của chúng tôi và là người đầu tiên biết về các cập nhật của chúng tôi",
     days: "Ngày",
     hours: "Giờ",
     minutes: "Phút",
-    seconds: "Giây"
+    seconds: "Giây",
   },
-} as any;
+};
